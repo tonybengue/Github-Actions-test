c6bc5fdf4694463f524eddffc297e860f50a228b
Tony Bengué <tonybengue@hotmail.fr>
2023-11-24 20:02:02 +0100

    Initial workflow for test

---
+++ math.test.js
@@ -0,0 +1,16 @@
+const { add, substract, divide, multiply } = require('./math')
+
+describe("Math.js tests", () => {
+  it("Should test add", () => {
+    expect(add(2, 2)).toBe(4)
+  })
+  it("Should test substract", () => {
+    expect(substract(2, 2)).toBe(0)
+  })
+  it("Should test multiply", () => {
+    expect(multiply(2, 2)).toBe(4)
+  })
+  it("Should test divide", () => {
+    expect(divide(2, 2)).toBe(1)
+  })
+})
